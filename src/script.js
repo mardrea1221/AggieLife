363b10fd213dc9fc0d76eecad319900238d1d13e
Joshua Buhain <jbuhain1@gmail.com>
2023-02-19 17:44:38 -0800

    added barebones code

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const filterBtn = document.getElementById('filter-btn');
+const filterDropdown = document.getElementById('filter-dropdown');
+
+filterBtn.addEventListener('click', function() {
+  console.log("hi")
+  if (filterDropdown.style.display === 'block') {
+    filterDropdown.style.display = 'none';
+    
+  } else {
+    filterDropdown.style.display = 'block';
+  }
+});
