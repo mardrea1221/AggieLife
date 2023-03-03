0ffc30a705525a716c95ac097c5c7f93e44c5810
Andrea Rodriguez <amvrodriguez@ucdavis.edu>
2023-03-03 00:39:02 -0800

    Added filter logo + still need to fix nav.html

--- src/script.js
+++ src/script.js
@@ -25,7 +25,6 @@ const filterBtn = document.getElementById('filter-btn');
 const filterDropdown = document.getElementById('filter-dropdown');
 
 filterBtn.addEventListener('click', function() {
-  // console.log("hi")
   if (filterDropdown.style.display === 'block') {
     filterDropdown.style.display = 'none';
     
